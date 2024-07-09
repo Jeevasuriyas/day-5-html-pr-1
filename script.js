79442febb70233ca4edd71bb16fbeaa4d6b080d6
JEEVA SURIYS S <jeevasuriya2001@gmail.com>
2024-07-09 16:27:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('off').addEventListener('click',function(){
+    document.getElementById("image").src="https://lh4.googleusercontent.com/proxy/_uidRTjpObkcMAT8rj1E6Zxs6tE5dae_ZV4iYcNbE9Qy3AVqfgjBZ-YqzNce5RZMMj4cO5JhOc4VfXnujjqTsxlQkiYG6CIo7npp0JaJtvq55NNNGh5FghEO-wVenpg-ogRGCJUH3-zsXtN0bmzKrQcnMwpm6yKs62IU8S5cucxFxHCEo7geA8815qxsiRDWkw"
+})
+document.getElementById('on').addEventListener('click',function(){
+    document.getElementById("image").src="https://c4.wallpaperflare.com/wallpaper/916/420/2/the-flash-season-3-grant-gustin-wallpaper-preview.jpg"
+})
